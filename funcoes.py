937cb928f14a7fcc72268945dec1f2d22c293317
Arthur <jaldsonarthur1322@gmail.com>
2025-11-23 19:59:17 -0300

    v1.5: criei a classe Cliente no funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,15 @@
-print("funcoes")
+import heapq
+from collections import deque 
+
+class Cliente: 
+    def __init__(self, nome, grau_prioride):
+        self.nome = nome;
+        self.grau_prioridade = grau_prioride;
+        
+    def __lt__(self,other):
+        return self.grau_prioridade < other.grau_prioridade;
+    
+    def __repr__(self):
+        tipo = "prioridade" if self.grau_prioridade < 5 else "prioridade normal"
+        return f"[{tipo} - Grau {self.grau_prioridade}] {self.nome}"
+
